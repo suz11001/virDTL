826d329b548f574ff211114a197a3b577293aede
Sumaira Zaman <sumaira.zaman@uconn.edu>
2021-08-16 14:14:36 -0400

    split step 1

--- 0_fetch_data/1_fetchFasta.sh
+++ 0_fetch_data/1_fetchFasta.sh
@@ -1,6 +1,3 @@
-#activate the conda environment in which entrez is installed
-source activate  ~/.conda/envs/entrez
-
 while read line <&3; do
     echo "$line"
     esearch -db nucleotide -query $line | efetch -format fasta > outputs/1_genomes/$line.fna
