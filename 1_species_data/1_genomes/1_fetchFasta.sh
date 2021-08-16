ca0c58863ea46e4593f2c10b9f9c1d1037f9ba05
Sumaira Zaman <sumaira.zaman@uconn.edu>
2021-08-16 00:37:26 -0400

    added genomes

--- 1_species_data/1_genomes/1_fetchFasta.sh
+++ 1_species_data/1_genomes/1_fetchFasta.sh
@@ -1,4 +1,7 @@
+#activate the conda environment in which entrez is installed
+source activate  ~/.conda/envs/entrez
+
 while read line <&3; do
     echo "$line"
-    esearch -db nucleotide -query $line | efetch -format fasta > genomes/$line.fna
+    esearch -db nucleotide -query $line | efetch -format fasta > outputs/1_genomes/$line.fna
 done 3<./input/genomes.txt
--- 1_species_data/1_genomes/2_fetchCDS.sh
+++ 1_species_data/1_genomes/2_fetchCDS.sh
@@ -1,4 +1,5 @@
+source activate  ~/.conda/envs/entrez
 while read line <&3; do
     echo "$line"
-    esearch -db nucleotide -query $line | efetch -format fasta_cds_na > cds/$line.fna
+    esearch -db nucleotide -query $line | efetch -format fasta_cds_na > outputs/2_cds/$line.fna
 done 3<./input/genomes.txt
