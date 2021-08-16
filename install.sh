e646b86a1a33ceeec52111dae2e9504e62712608
Samuel Sledzieski <samsledje@gmail.com>
2021-08-16 14:09:12 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/bash
+echo Installing `virDTL` conda environment...
+if $(conda env list | grep -q virDTL)
+then
+    conda deactivate
+    conda activate virDTL
+else
+    conda deactivate
+    conda env create -f environment.yml
+    conda activate virDTL
+fi
+
 for i in $(find software/ranger | grep .linux)
 do
-    echo Copying $i to "$CONDA_PREFIX"/bin
+    echo Copying $i to "$CONDA_PREFIX"/bin...
     cp $i "$CONDA_PREFIX"/bin
 done
